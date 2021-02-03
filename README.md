4f42fc79a3b7945fcadbb5e5862712a0ebee6f86
amankumar6 <amankumar786dpsdh@gmail.com>
2021-02-03 14:36:30 +0530

    fix: typo + feat: meta tag + docs: Readme has been added

---
+++ README.md
@@ -0,0 +1,5 @@
+# 𝐵𝒶𝓈𝒾𝒸 𝒞𝒶𝓁𝒸𝓊𝓁𝒶𝓉𝑜𝓇
+
+![Hexagon](BasicCalculator.gif)
+
+It is a simple Basic Calculator that can do Addition, Subtraction, Multiplication, Division, and Percentage. Which is made using TypeScript.
--- script/calculator.ts
+++ script/calculator.ts
@@ -1,99 +1,111 @@
-const loader: any = document.getElementById("loading"),
-    operator: any = document.querySelectorAll(".operator"),
-    number: any = document.querySelectorAll(".number"),
-    input: any = document.querySelector(".custom-control-input"),
-    cal: any = document.querySelector(".main-calc"),
-    video: any = document.getElementById('video'),
-    ot: any = document.getElementById("output"),
-    empty: any = document.querySelectorAll(".empty");
+const loader: HTMLElement | null = document.querySelector('#loading'),
+    input: HTMLElement | null = document.querySelector('.custom-control-input'),
+    cal: HTMLElement | null = document.querySelector('.main-calc'),
+    video: HTMLVideoElement | null = document.querySelector('#video'),
+    ot: HTMLElement | null = document.querySelector('#output'),
+    operator: NodeListOf<HTMLElement> | null = document.querySelectorAll(
+        '.operator'
+    ),
+    number: NodeListOf<HTMLElement> | null = document.querySelectorAll(
+        '.number'
+    ),
+    empty: NodeListOf<HTMLElement> | null = document.querySelectorAll('.empty');
 
-let bool = false;
+let bool: boolean = false;
 
-window.addEventListener('load', () => loader.style.display = 'none');
+window.addEventListener('load', () => (loader.style.display = 'none'));
 
 function getHistory() {
-    return (document.getElementById("history-value") as HTMLInputElement).innerText;
+    return (document.getElementById('history-value') as HTMLInputElement)
+        .innerText;
 }
 
-function printHistory(num: any) {
-    (document.getElementById("history-value") as HTMLInputElement).innerText = num;
+function printHistory(num: string) {
+    (document.getElementById(
+        'history-value'
+    ) as HTMLInputElement).innerText = num;
 }
 
 function getOutput() {
-    return (document.getElementById("output-value") as HTMLInputElement).innerText;
+    return (document.getElementById('output-value') as HTMLInputElement)
+        .innerText;
 }
 
-function printOutput(num: any) {
-    (num == "") ? (document.getElementById("output-value") as HTMLInputElement).innerText = num : (document.getElementById("output-value") as HTMLInputElement).innerText = getFormattedNumber(num);
+function printOutput(num: string) {
+    num == ''
+        ? ((document.getElementById(
+              'output-value'
+          ) as HTMLInputElement).innerText = num)
+        : ((document.getElementById(
+              'output-value'
+          ) as HTMLInputElement).innerText = getFormattedNumber(num));
 }
 
-function getFormattedNumber(num: any) {
-    if (num == "-") {
-        return "";
-    }
-    let n = Number(num);
-    let value = n.toLocaleString("en");
+function getFormattedNumber(num: string) {
+    if (num == '-') return '';
+    let n = Number(num),
+        value = n.toLocaleString('en');
     return value;
 }
 
-function reverseNumberFormat(num: any) {
+function reverseNumberFormat(num: string) {
     return Number(num.replace(/,/g, ''));
 }
 
-operator.forEach((element: any) => {
-    element.addEventListener('click', function () {
-        if (this.id == "clear") {
-            printHistory("");
-            printOutput("");
-        } else if (this.id == "backspace") {
-            let output: any = reverseNumberFormat(getOutput()).toString();
+operator.forEach((element: HTMLElement | null) => {
+    element.addEventListener('click', () => {
+        if (this.id == 'clear') {
+            printHistory('');
+            printOutput('');
+        } else if (this.id == 'backspace') {
+            let output: string = reverseNumberFormat(getOutput()).toString();
             if (output) {
                 output = output.substr(0, output.length - 1);
                 printOutput(output);
             }
         } else {
-            let output: any = getOutput();
-            let history: any = getHistory();
-            if (output == "" && history != "") {
-                if (isNaN(history[history.length - 1])) {
+            let output: any = getOutput(),
+                history: any = getHistory();
+            if (output == '' && history != '')
+                if (isNaN(history[history.length - 1]))
                     history = history.substr(0, history.length - 1);
-                }
-            }
-            if (output != "" || history != "") {
-                output = output == "" ? output : reverseNumberFormat(output);
+
+            if (output != '' || history != '') {
+                output = output == '' ? output : reverseNumberFormat(output);
                 history += output;
-                if (this.id == "=") {
+                if (this.id == '=') {
                     let result = eval(history);
                     printOutput(result);
-                    printHistory("");
+                    printHistory('');
                 } else {
                     history += this.id;
                     printHistory(history);
-                    printOutput("");
+                    printOutput('');
                 }
             }
         }
-
     });
-})
+});
 
 number.forEach((element: any) => {
-    element.addEventListener('click', function () {
-        let output = reverseNumberFormat(getOutput());
+    element.addEventListener('click', () => {
+        let output: any = reverseNumberFormat(getOutput());
         if (output != NaN) {
             output += this.id;
             printOutput(output);
         }
     });
-})
+});
 
-video.src = "https://res.cloudinary.com/dbvthtwhc/video/upload/v1605202323/Basic-Calculator/light_b1jros.webm";
+video.src =
+    'https://res.cloudinary.com/dbvthtwhc/video/upload/v1605202323/Basic-Calculator/light.webm';
 video.play();
 
-input.addEventListener("change", () => {
+input.addEventListener('change', () => {
     if (bool) {
         video.pause();
-        video.src = "https://res.cloudinary.com/dbvthtwhc/video/upload/v1605202323/Basic-Calculator/light_b1jros.webm";
+        video.src =
+            'https://res.cloudinary.com/dbvthtwhc/video/upload/v1605202334/Basic-Calculator/dark.mp4';
         video.load();
         video.play();
         document.documentElement.style.setProperty('--bgColor', '#eaedef');
@@ -101,7 +113,8 @@ input.addEventListener("change", () => {
         document.documentElement.style.setProperty('--number', '#7d93e0');
     } else {
         video.pause();
-        video.src = "https://res.cloudinary.com/dbvthtwhc/video/upload/v1605202334/Basic-Calculator/dark_wcbgar.mp4"
+        video.src =
+            'https://res.cloudinary.com/dbvthtwhc/video/upload/v1605202334/Basic-Calculator/dark_wcbgar.mp4';
         video.load();
         video.play();
         document.documentElement.style.setProperty('--bgColor', '#1b2838');
@@ -109,4 +122,4 @@ input.addEventListener("change", () => {
         document.documentElement.style.setProperty('--number', '#333');
     }
     bool = !bool;
-})
+});
